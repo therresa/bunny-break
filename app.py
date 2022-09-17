d42a3a233a564d830b944c7526081c66a691ac1c
xmaggieliu <xmaggie.liu@gmail.com>
2022-09-17 10:59:02 -0400

    centrered title

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
+    inp = request.form.get('text-chat')
+    
+
